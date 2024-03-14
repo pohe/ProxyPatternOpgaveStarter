74b07bc010f3c3ba1423065a8dc912ae5e8070e1
Poul Henriksen <POHE@zealand.dk>
2024-03-14 08:35:35 +0100

    Add project files.

---
+++ ProxyPatternOpgaveStarter/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProxyPatternOpgaveStarter;
+
+Console.WriteLine("Hello, Proxy design pattern!");
+
+
+//IImage image1 = new ImageProxy("image1.jpg");
+//IImage image2 = new ImageProxy("image2.jpg");
+
+//// Image1 will be loaded from disk
+//image1.Display();
+//// Image1 will not be loaded again, as it's already loaded
+//image1.Display();
+//// Image2 will be loaded from disk
+//image2.Display();
---
+++ ProxyPatternOpgaveStarter/RealImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPatternOpgaveStarter
+{
+    // Concrete implementation of Image
+    class RealImage : IImage
+    {
+        private String filename;
+
+        public RealImage(String filename)
+        {
+            this.filename = filename;
+            loadFromDisk();
+        }
+
+        private void loadFromDisk()
+        {
+            Console.WriteLine("Loading image " + filename);
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Displaying image " + filename);
+        }
+
+}
